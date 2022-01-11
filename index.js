4de7875776aa74c9b99f7f5d88432659280d2126
goncalofeijo7 <feijoreact@gmail.com>
2022-01-11 19:30:33 -0100

    Criação da pagina visita/explora

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+ 
+app.use(express.static('./public'))
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'./index.html'))
+})
+ 
+const port = 3000
+
+app.listen(port, () => {
+    console.log('Listenning...')
+})
