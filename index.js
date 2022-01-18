3a20c00e69145423b5e1cfb5cd25ad8050d4063d
Soares17 <goncaloenta2019.20@gmail.com>
2022-01-18 10:01:34 -0100

    more scripts

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.use(express.static('./public'))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'./index.html'))
 })
- 
+
+app.get('/navbar', (req, res) =>{
+ res.sendFile(path.join(__dirname,'./navbar.html'))
+})
+
 const port = 3000
 
 app.listen(port, () => {
